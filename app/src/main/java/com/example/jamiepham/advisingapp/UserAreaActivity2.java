9082130264888b7a225c7aa4288eafac23cee83c
swiftandquick <ychomework4@gmail.com>
2017-04-24 16:47:28 -0400

    Update UserAreaActivity2.java

--- app/src/main/java/com/example/jamiepham/advisingapp/UserAreaActivity2.java
+++ app/src/main/java/com/example/jamiepham/advisingapp/UserAreaActivity2.java
@@ -1,18 +1,30 @@
 package com.example.jamiepham.advisingapp;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class UserAreaActivity2 extends AppCompatActivity {
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area2);
+        super.onCreate(savedInstanceState);
+        mHandler.postDelayed(new Runnable() {
+            public void run() {
+                doStuff();
+            }
+        }, 60000);
+    }
 
+    private void doStuff() {
+        Intent intent = new Intent(UserAreaActivity2.this, LoginActivity.class);
+
+        startActivity(intent);
 
         Button backtoUserActivity = (Button) findViewById(R.id.Previous);
         backtoUserActivity.setOnClickListener(new View.OnClickListener() {
